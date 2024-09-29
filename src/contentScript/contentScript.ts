8d1ed0a2fac9204d1bebf341e78c76cad2ee0bb9
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-09-29 06:36:42 +0530

    [add] basco data extracion from zara

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,4 +1,43 @@
-window.onload = function() {
-    console.log('contentScript loaded');
-    // do something
+function extractAllProductInfo() {
+    const productElements = document.querySelectorAll('.product-grid-product');
+    const allProductInfo = [];
+    console.log("length",productElements.length);
+    if(productElements.length == 0)
+      return extractAllProductInfo();
+
+    // Loop through each product element and extract details
+    productElements.forEach(productElement => {
+        const productNameElement = productElement.querySelector('h2');
+        const productPriceElement = productElement.querySelector('.price__amount-wrapper .money-amount__main') ;
+        const productImageElement = productElement.querySelector('.media-image__image');
+
+        // Extract the product name
+        const productName = productNameElement.innerText;
+
+        // Extract the product price
+        const productPrice = productPriceElement.textContent;
+
+        // Extract the product image URL (First image from the carousel)
+        const productImage = (productImageElement as HTMLImageElement)?.src;
+
+
+        // Store the extracted information in an object
+        const productInfo = {
+            name: productName,
+            price: productPrice,
+            imageUrl: productImage
+        };
+
+        // Push the product info to the allProductInfo array
+        allProductInfo.push(productInfo);
+    });
+
+    return allProductInfo;
 }
+
+
+window.onload = function() {
+    const productsInfo = extractAllProductInfo();
+    console.log('Extracted Product Information:', productsInfo);
+}
+
